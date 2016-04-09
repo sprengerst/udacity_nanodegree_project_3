af716f648a03e6be5dced73360bf8636e73b1277
stefan <stefansprenger@gmx.at>
2016-04-09 21:17:39 +0200

    refactor StockDetailFragment

--- app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailFragment.java
+++ app/src/main/java/com/sam_chordas/android/stockhawk/ui/StockDetailFragment.java
@@ -5,7 +5,6 @@
 package com.sam_chordas.android.stockhawk.ui;
 
 import android.database.Cursor;
-import android.database.DatabaseUtils;
 import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
@@ -31,8 +30,6 @@ import java.util.Map;
 public class StockDetailFragment extends Fragment {
 
     public static final String DETAIL_URI = "DETAIL_URI";
-
-
     public StockDetailFragment() {
         setHasOptionsMenu(true);
     }
@@ -47,45 +44,45 @@ public class StockDetailFragment extends Fragment {
 
         if (arguments != null) {
             Uri symbolUri = arguments.getParcelable(StockDetailFragment.DETAIL_URI);
+            assert symbolUri != null;
             String symbol = symbolUri.getLastPathSegment();
             Map<String, Float> chartMap = new LinkedHashMap<>();
 
-            Cursor c = getContext().getContentResolver().query(symbolUri,
+            Cursor chartEntriesCursor = getContext().getContentResolver().query(symbolUri,
                     new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
                             QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP,
                             QuoteColumns.CREATEDATE},   QuoteColumns.ISCURRENT + "= ?"+ ") GROUP BY (createdate",
                     new String[]{"0"}, QuoteColumns._ID + " ASC");
 
-            DatabaseUtils.dumpCursor(c);
+            // fill chartMap with cursor entries
+            if(chartEntriesCursor != null) {
+                while (chartEntriesCursor.moveToNext()) {
+                    float bidPrice = Float.parseFloat(chartEntriesCursor.getString(chartEntriesCursor.getColumnIndex(QuoteColumns.BIDPRICE)).replace(",", "."));
+                    String createdDate = chartEntriesCursor.getString(chartEntriesCursor.getColumnIndex(QuoteColumns.CREATEDATE));
+                    chartMap.put(createdDate, bidPrice);
+                }
 
-
-            while (c.moveToNext()) {
-                System.out.println("USED ENTRY: " + c.getString(c.getColumnIndex(QuoteColumns._ID)));
-                System.out.println("USED SYMBOL: " + c.getString(c.getColumnIndex(QuoteColumns.SYMBOL)));
-                float bidPrice = Float.parseFloat(c.getString(c.getColumnIndex(QuoteColumns.BIDPRICE)).replace(",", "."));
-                String createdDate = c.getString(c.getColumnIndex(QuoteColumns.CREATEDATE));
-                System.out.println("CREATEDATE:" + createdDate);
-                chartMap.put(createdDate, bidPrice);
+                chartEntriesCursor.close();
             }
 
-
-            Cursor cursorActual = getContext().getContentResolver().query(symbolUri,
+            // add today entry
+            Cursor cursorToday = getContext().getContentResolver().query(symbolUri,
                     new String[]{QuoteColumns._ID, QuoteColumns.SYMBOL, QuoteColumns.BIDPRICE,
                             QuoteColumns.PERCENT_CHANGE, QuoteColumns.CHANGE, QuoteColumns.ISUP}, QuoteColumns.ISCURRENT + "= ?",
                     new String[]{"1"}, null);
 
-            cursorActual.moveToFirst();
+            if(cursorToday!= null && cursorToday.getCount()!=0) {
+                cursorToday.moveToFirst();
+                chartMap.put(getActivity().getString(R.string.chart_actual_text), Float.parseFloat(cursorToday.getString(cursorToday.getColumnIndex(QuoteColumns.BIDPRICE)).replace(",", ".")));
+                cursorToday.close();
+            }
 
-            chartMap.put("Actual", Float.parseFloat(cursorActual.getString(cursorActual.getColumnIndex(QuoteColumns.BIDPRICE)).replace(",", ".")));
-
-            LineChart mLineChartView = (LineChart) rootView.findViewById(R.id.linechartsstock);
-
+            // fill chart with data
             StringBuilder contentDescriptionChart = new StringBuilder();
-            contentDescriptionChart.append("This chart shows the stock history for the stock symbol "+symbol);
+            contentDescriptionChart.append(getActivity().getString(R.string.accessibly_chart_intro)).append(symbol);
 
             float minVal = Float.MAX_VALUE;
             float maxVal = 0;
-
 
             List<Entry> data = new ArrayList<>();
             List<String> labels = new ArrayList<>();
@@ -93,7 +90,7 @@ public class StockDetailFragment extends Fragment {
             int i = 0;
             for (Map.Entry<String, Float> historicEntry : chartMap.entrySet()) {
 
-                contentDescriptionChart.append("Stock closing value on date " + historicEntry.getKey() + " is " + historicEntry.getValue() + ".");
+                contentDescriptionChart.append(getActivity().getString(R.string.accessiblity_chart_value)).append(historicEntry.getKey()).append(" = ").append(historicEntry.getValue()).append(".");
 
                 data.add(new Entry(historicEntry.getValue(), i));
                 labels.add(historicEntry.getKey());
@@ -108,16 +105,16 @@ public class StockDetailFragment extends Fragment {
                 i++;
             }
 
-            System.out.println("DATA LENGTH: " + data.size());
+            LineDataSet lineDataSet = new LineDataSet(data, getActivity().getString(R.string.chart_points_text));
 
-            LineDataSet dataset = new LineDataSet(data, "Bid Price");
+            lineDataSet.setDrawCubic(true);
+            lineDataSet.setDrawFilled(true);
 
-            dataset.setDrawCubic(true);
-            dataset.setDrawFilled(true);
-
-            LineData combinedLineData = new LineData(labels, dataset);
+            LineData combinedLineData = new LineData(labels, lineDataSet);
 
             combinedLineData.setValueTextColor(Color.WHITE);
+
+            LineChart mLineChartView = (LineChart) rootView.findViewById(R.id.linechartsstock);
 
             XAxis xAxis = mLineChartView.getXAxis();
             xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
@@ -129,13 +126,11 @@ public class StockDetailFragment extends Fragment {
             mLineChartView.getAxisLeft().setTextColor(Color.WHITE);
             mLineChartView.getAxisRight().setTextColor(Color.WHITE);
             mLineChartView.setData(combinedLineData);
-            mLineChartView.setDescription("Overview for Stock: " + symbol);
+            mLineChartView.setDescription(getActivity().getString(R.string.chart_description) + symbol);
             mLineChartView.setDescriptionColor(Color.WHITE);
             mLineChartView.setBorderColor(Color.WHITE);
 
             mLineChartView.setContentDescription(contentDescriptionChart.toString());
-
-
 
         }
 
